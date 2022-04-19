d6d3f99c5581fb704e86dc1097d335da01f6408c
MavosV8 <lukaszpasi@gmail.com>
2022-04-19 21:59:35 +0200

    test change

--- src/app/table-lister/table-lister.component.ts
+++ src/app/table-lister/table-lister.component.ts
@@ -4,6 +4,8 @@ import {DbRequestService} from "../message-service/db-request.service";
 import {Observable} from "rxjs";
 import {HttpErrorResponse} from "@angular/common/http";
 import {SignoutButtonComponent} from "../signout-button/signout-button.component";
+import {SignoutService} from "../signout-service/signout.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-table-lister',
@@ -13,10 +15,20 @@ import {SignoutButtonComponent} from "../signout-button/signout-button.component
 export class TableListerComponent implements OnInit {
   tables: Table[] =  [{id: 0, name : "Null table"}];
 
-  constructor(private dbRequester : DbRequestService) {
+  constructor(private dbRequester : DbRequestService, public signoutService: SignoutService, public router : Router ) {
   }
 
   ngOnInit(): void {
+  }
+
+  logout() : void {
+    console.log("button works but not logout")
+    this.signoutService.logout().subscribe(result => {
+      if(result.result == "success"){
+        this.router.navigateByUrl("/");
+        console.log("button works");
+      }
+    });
   }
 
   getTables() : void {
